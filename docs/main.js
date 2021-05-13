00faf80382c50afcee75f179a240b2e4df69300d
Ваше Имя <you@example.com>
2021-05-13 19:04:55 -0300

    microfix

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,7 @@
-console.log(window.location);
+// console.log(window.location);
 
-body.style.color = "blue";
+// body.style.color = "blue";
+
+if (location.pathname == "") {
+  $("body").css("color", "red");
+}
